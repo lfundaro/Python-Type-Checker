987730f2061f05aa1e92c99b4f8f63eecfc8608e
Marion <Mundos Graves@.(none)>
2010-04-08 16:55:57 -0430

    Por fin unificacion sirviendo

--- Operaciones.py
+++ Operaciones.py
@@ -77,24 +77,24 @@ def unif(tipo1, tipo2):
         else: return [(tipo1, tipo2)]
         
 	# tipo1 es un entero, unifica consigo mismo.
-	if isinstance(tipo1,Int):
-            if isinstance(tipo2,Int) and tipo1.valor == tipo2.valor: return []
-            else: return 'Error'
+    if isinstance(tipo1,Int):
+        if isinstance(tipo2,Int): return [(tipo1,tipo2)]
         else: return 'Error'
         
-        # tipo1 es un booleano, unifica consigo mismo
-        if isinstance(tipo1,Bool):
-            if isinstance(tipo2,Bool) and tipo1.valor == tipo2.valor: return []
-            else: return 'Error'
+    
+	# tipo1 es un booleano, unifica consigo mismo
+    if isinstance(tipo1,Bool):
+        if isinstance(tipo2,Bool): return [(tipo1,tipo2)]
         else: return 'Error'
-	
+      
         # tipo1 es una funcion y tipo2 es una funcion.
-        if isinstance(tipo1, Tipo_Funcion): 
-            if isinstance(tipo2, Tipo_Funcion):
-                w = unif(tipo1.T1, tipo2.T1)
-                resultado = componer([w], unif(sustituir([w], tipo1.T2), sustituir([w], tipo2.T2)))
-                return resultado
-            elif isinstance(tipo2, Var_tipo):
-                return unif(tipo2, tipo1)
-            elif isinstance(tipo2, Bool) or isinstance(tipo2,Int):
-                return unif(tipo2,tipo1)
+    if isinstance(tipo1, Tipo_Funcion): 
+        if isinstance(tipo2, Tipo_Funcion):
+            w = unif(tipo1.T1, tipo2.T1)
+            resultado = componer([w], unif(sustituir([w], tipo1.T2), sustituir([w], tipo2.T2)))
+            return resultado
+        elif isinstance(tipo2, Var_tipo):
+            return unif(tipo2, tipo1)
+        elif isinstance(tipo2, Bool) or isinstance(tipo2,Int):
+            return unif(tipo2,tipo1)
+        
